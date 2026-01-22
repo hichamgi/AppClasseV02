821eb6c77fa8d8ce16c93c8a3c70be769270479d
Hicham <hichamgi@gmail.com>
2026-01-22 19:26:53 +0100

    Ajout favicon

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -13,6 +13,7 @@ $user = Auth::user();
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AppClasseV02</title>
+  <link rel="icon" type="image/svg+xml" href="<?= $baseUrl ?>/favicon.svg">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
